01a333ff9a349eca1c2cf500676ceac7c48eebf8
rjafc-iscteiul <rjafc@iscte.pt>
2020-03-24 17:19:18 +0000

    Changed pom to Junit5.

--- Grupo37ESII/src/main/java/Group37ESII/Calculator.java
+++ Grupo37ESII/src/main/java/Group37ESII/Calculator.java
@@ -1,13 +1,43 @@
 package Group37ESII;
 
 /**
- * Hello world!
+ * @author Group37 ESII
+ * @version 1.0
+ * @since 22 march 2020
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+
+public class Calculator {
+    
+	
+	/**
+	 * @param num1 The first parameter of add method.
+	 * @param num2 The second parameter of add method.
+	 * @return The sum of num1 and num2
+	 */
+	public int add(int num1, int num2) {
+		return num1+num2;
+	}
+	
+	
+	/**
+	 * @param num1 The first parameter of subtract method.
+	 * @param num2 The second parameter of subtract method.
+	 * @return The subtraction of num1 by num2 (in that order)
+	 */
+	public int subtract(int num1, int num2) {
+		return num1-num2;
+	}
+	
+	
+	/**
+	 * @param num1 The first parameter of multiplication method.
+	 * @param num2 The second parameter of multiplication method.
+	 * @return The multiplication of num1 by num2.
+	 */
+	public int multiplication(int num1, int num2) {
+		return num1*num2;
+	}
+	
 }
